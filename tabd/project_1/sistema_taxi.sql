8f20457b4b7669d0548c0b74d5ce00fb6034876c
Devwarlt <nadio.pontes@redes.unb.br>
2021-03-24 21:37:32 -0300

    Update sistema_taxi.sql

--- tabd/project_1/sistema_taxi.sql
+++ tabd/project_1/sistema_taxi.sql
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `taxi` (
     `placa` VARCHAR(8) NOT NULL,
     `marca` VARCHAR(32) NOT NULL,
     `modelo` VARCHAR(32) NOT NULL,
-    `ano_fabricacao` VARCHAR(4),
+    `ano_fabricacao` VARCHAR(4) NOT NULL,
     PRIMARY KEY (`placa`)
 )  ENGINE=INNODB;
 
